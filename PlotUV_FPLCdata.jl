9ad681c8abc6513025d01218b6604c925712b01f
IMAdams <141898123+IMAdams@users.noreply.github.com>
2024-11-04 13:30:05 -0700

    Updates

--- PlotUV_FPLCdata.jl
+++ PlotUV_FPLCdata.jl
@@ -6,7 +6,7 @@ using DataFrames
 # Pkg.add("CairoMakie")
 using CairoMakie
 
-df = CSV.read("C:\\Users\\imadams\\OneDrive - University of New Mexico Health Sciences Center\\Desktop\\20240925_HIS_HA_EGFR.csv", DataFrame; header=2, skipto = 10)
+df = CSV.read("D:LidkeLab\\FPLC\\20240925_HIS_HA_EGFR.csv", DataFrame; header=2, skipto = 10)
 
 fig = Figure()
 ax = Axis(fig[1,1], xlabel= "Column Volume", ylabel = "mAU(Abs)", 
@@ -17,4 +17,4 @@ lines!(ax, df."UV1 (280 nm)_volume", df."UV1 (280 nm)_mAU"; label = "280 nm")
 # lines!(ax, df."UV1 (215 nm)_volume", df."UV4 (495 nm)_mAU"; label = "495 nm")
 axislegend(ax; position = :rt)
 display(fig)
-save("C:\\Users\\imadams\\OneDrive - University of New Mexico Health Sciences Center\\Desktop\\20240920_HIS_HA_EGFR.png", fig)
+# save("C:\\Users\\imadams\\OneDrive - University of New Mexico Health Sciences Center\\Desktop\\20240920_HIS_HA_EGFR.png", fig)
